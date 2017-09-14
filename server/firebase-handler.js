85afa16f74d3e113b16025df233243eb17450848
Cihad Turhan <cihadturhan@gmail.com>
2017-09-14 23:48:12 +0300

    code refactoring

--- server/firebase-handler.js
+++ server/firebase-handler.js
@@ -19,21 +19,21 @@ const db = admin.database();
 const ref = db.ref("devices");
 
 
-function insertDevice(connection, snapshot){
+function insertDevice(connection, snapshot) {
     let device = Object.assign({firebaseId: snapshot.key}, snapshot.val());
     connection.query(`INSERT INTO ${TABLE_DEVICE} SET ?`, device, function (error, results) {
         if (error) {
-            throw error
+            throw error;
         }
         console.log(`DEVICE with id ${results.insertId} ADDED!`);
     });
 }
 
-function updateDevice(connection, snapshot){
+function updateDevice(connection, snapshot) {
     let device = Object.assign({updatedAt: moment().format(DATETIME_FORMAT)}, snapshot.val());
     let query = connection.query(`UPDATE ${TABLE_DEVICE} SET ? WHERE firebaseId = ?`, [device, snapshot.key], function (error) {
         if (error) {
-            throw error
+            throw error;
         }
 
         console.log(`DEVICE with id ${snapshot.key} UPDATED!`);
@@ -44,17 +44,21 @@ function updateDevice(connection, snapshot){
 
 function deleteDevice(connection, snapshot) {
     connection.query(`DELETE FROM ${TABLE_DEVICE} WHERE firebaseId=?`, [snapshot.key], function (error) {
-        if (error) throw error;
+        if (error) {
+            throw error;
+        }
         console.log(`DEVICE with id ${snapshot.key} REMOVED!`);
     });
 }
 
 // Attach an asynchronous callback to read the data at our posts reference
-ref.on("child_added", function(snapshot) {
+ref.on("child_added", function (snapshot) {
 
     MySqlHandler.get().then(connection => {
         connection.query(`SELECT * FROM ${TABLE_DEVICE} WHERE firebaseId=?`, snapshot.key, function (error, results) {
-            if (error) throw error;
+            if (error) {
+                throw error;
+            }
             if (results.length === 0) {
                 insertDevice(connection, snapshot);
             }
@@ -65,7 +69,7 @@ ref.on("child_added", function(snapshot) {
 });
 
 
-ref.on("child_changed", function(snapshot) {
+ref.on("child_changed", function (snapshot) {
     MySqlHandler.get().then(connection => {
         updateDevice(connection, snapshot);
     });
@@ -73,9 +77,7 @@ ref.on("child_changed", function(snapshot) {
     throw errorObject;
 });
 
-ref.on("child_removed", function(snapshot) {
-    let device = Object.assign({firebaseId: snapshot.key}, snapshot.val());
-
+ref.on("child_removed", function (snapshot) {
     MySqlHandler.get().then(connection => {
         deleteDevice(connection, snapshot);
     });
