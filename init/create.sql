7796472d61d35d89a6033443ffd59895c84785c0
Cihad Turhan <cihadturhan@gmail.com>
2017-09-15 00:05:58 +0300

    foreign key added for device

--- init/create.sql
+++ init/create.sql
@@ -1,3 +1,6 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `playlist`;
 CREATE TABLE `playlist` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -7,6 +10,7 @@ CREATE TABLE `playlist` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `group`;
 CREATE TABLE `group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -15,6 +19,7 @@ CREATE TABLE `group` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `device`;
 CREATE TABLE `device` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -23,14 +28,17 @@ CREATE TABLE `device` (
   `groupId` int(11) DEFAULT NULL,
   `firebaseId` varchar(24) NOT NULL,
   `appVersion` int(11) DEFAULT NULL,
-  `device` varchar(255) DEFAULT NULL,
+  `device` varchar(128) DEFAULT NULL,
   `lastOnline` bigint(20) DEFAULT NULL,
   `osVersion` int(11) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `firebaseId` (`firebaseId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `firebaseId` (`firebaseId`),
+  KEY `Device - Group` (`groupId`),
+  CONSTRAINT `Device - Group` FOREIGN KEY (`groupId`) REFERENCES `group` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `media`;
 CREATE TABLE `media` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
